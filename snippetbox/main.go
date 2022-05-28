8a71032786414426bdcd82160dcfaccbdc443d76
mahmud139 <mahmudulhasan139139@gmail.com>
2022-05-28 19:49:36 +0600

    add more handler

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -15,9 +15,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a new snippet"))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Starting server on localhost:4000")
 	err := http.ListenAndServe(":4000", mux)

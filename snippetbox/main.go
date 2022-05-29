2e5f3e116971e643d51d688ad73728d7623c795e
mahmud139 <mahmudulhasan139139@gmail.com>
2022-05-29 22:16:16 +0600

    http status code added to createSnippet function

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -24,6 +24,12 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(405)
+		w.Write([]byte("Method not Allowed"))
+		return
+	}
+
 	w.Write([]byte("Create a new snippet"))
 }
 

069267fde01080099be0cce4272597884bec8620
mahmud139 <mahmudulhasan139139@gmail.com>
2022-06-07 22:52:24 +0600

    adding the fileServer

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -17,6 +17,9 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	fileServer := http.FileServer(http.Dir("M:/code_of_Golang/go_workspace/src/projects/snippetbox/ui/static"))
+	mux.Handle("/static/",http.StripPrefix("/static", fileServer))
+
 	log.Println("Starting Server on localhost:8080")
 	err := http.ListenAndServe("localhost:8080", mux)
 	checkErr(err)

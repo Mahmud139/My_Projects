589144a640caf38c678557ff5cc2280c1c459a9b
mahmud139 <mahmudulhasan139139@gmail.com>
2022-07-12 16:11:27 +0600

    adding common dynamic data

--- snippetbox/cmd/web/helpers.go
+++ snippetbox/cmd/web/helpers.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"runtime/debug"
+	"time"
 )
 
 func (app *application) serverError(w http.ResponseWriter, err error) {
@@ -23,6 +24,14 @@ func (app *application) notFound(w http.ResponseWriter) {
 	app.clientError(w, http.StatusNotFound)
 }
 
+func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
+	if td == nil {
+		td = &templateData{}
+	}
+	td.CurrentYear = time.Now().Year()
+	return td
+}
+
 func (app *application) render(w http.ResponseWriter, r *http.Request, name string, td *templateData){
 	// Retrieve the appropriate template set from the cache based on the page name 
 	// (like 'home.page.tmpl'). If no entry exists in the cache with the 
@@ -35,7 +44,7 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 
 	buf :=new(bytes.Buffer)
 
-	err := ts.Execute(buf, td)
+	err := ts.Execute(buf, app.addDefaultData(td, r))
 	if err != nil {
 		app.serverError(w, err)
 	}
--- snippetbox/cmd/web/templates.go
+++ snippetbox/cmd/web/templates.go
@@ -8,6 +8,7 @@ import (
 )
 
 type templateData struct {
+	CurrentYear int
 	Snippet *models.Snippet
 	Snippets []*models.Snippet
 }

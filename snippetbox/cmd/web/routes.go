75edbc2c5666c3509665d6271e8b04a72da39d79
mahmud139 <mahmudulhasan139139@gmail.com>
2022-07-15 15:52:53 +0600

    using justinas/alice package for painless middleware chaining

--- snippetbox/cmd/web/routes.go
+++ snippetbox/cmd/web/routes.go
@@ -1,10 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 // Update the signature for the routes() method so that it returns a 
 // http.Handler instead of *http.ServeMux.
 func (app *application) routes() http.Handler {
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+	
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
@@ -13,5 +19,5 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("M:/code_of_Golang/go_workspace/src/projects/snippetbox/ui/static"))
 	mux.Handle("/static/",http.StripPrefix("/static", fileServer))
 
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	return standardMiddleware.Then(mux)
 }

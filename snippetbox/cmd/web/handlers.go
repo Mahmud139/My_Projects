4a170bb50875cf7b0eb489615c95bb1564ab88b9
mahmud139 <mahmudulhasan139139@gmail.com>
2022-07-17 17:23:58 +0600

    displaying validation error

--- snippetbox/cmd/web/handlers.go
+++ snippetbox/cmd/web/handlers.go
@@ -176,7 +176,10 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if len(errors) > 0 {
-		fmt.Fprint(w, errors)
+		app.render(w, r, "create.page.tmpl", &templateData{
+			FormErrors: errors,
+			FormData: r.PostForm,
+		})
 		return
 	}
 

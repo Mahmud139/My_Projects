6ce8992c553ebf323cf6683dc225e7d7ce760b47
mahmud139 <mahmudulhasan139139@gmail.com>
2022-07-20 08:27:49 +0600

    using session manager in practice

--- snippetbox/cmd/web/handlers.go
+++ snippetbox/cmd/web/handlers.go
@@ -78,7 +78,10 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	flash := app.session.PopString(r, "flash")
+
 	app.render(w, r, "show.page.tmpl", &templateData{
+		Flash: flash,
 		Snippet: s,
 	})
 
@@ -207,6 +210,8 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		app.serverError(w, err)
 		return
 	}
+
+	app.session.Put(r, "flash", "Snippet successfully created")
 	//w.Write([]byte("Create a new snippet"))
 	http.Redirect(w, r, fmt.Sprintf("/snippet/%d",id), http.StatusSeeOther)
 	/*The HTTP response status code 303 See Other is a way to redirect web applications 

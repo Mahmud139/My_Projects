708fdc93dd2945cb8255ecd66cf1b4d7f59a4c2b
mahmud139 <mahmudulhasan139139@gmail.com>
2022-07-23 17:29:21 +0600

    update main.go

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -34,6 +34,7 @@ type application struct {
 	session       *sessions.Session
 	snippets      *mysql.SnippetModel
 	templateCache map[string]*template.Template
+	users *mysql.UserModel
 }
 
 func main() {
@@ -82,6 +83,7 @@ func main() {
 		session:       session,
 		snippets:      &mysql.SnippetModel{DB: db},
 		templateCache: templateCache,
+		users: &mysql.UserModel{DB: db},
 	}
 
 	// Initialize a tls.Config struct to hold the non-default TLS settings we want 

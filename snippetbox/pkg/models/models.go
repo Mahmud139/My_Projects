825aee3e23123b47ae904417114a7c35997f54db
mahmud139 <mahmudulhasan139139@gmail.com>
2022-07-23 16:57:59 +0600

    update models

--- snippetbox/pkg/models/models.go
+++ snippetbox/pkg/models/models.go
@@ -5,7 +5,12 @@ import (
 	"time"
 )
 
-var ErrNoRecord = errors.New("models: no matching record found")
+var (
+	ErrNoRecord = errors.New("models: no matching record found")
+	ErrInvalidCredentials = errors.New("models: invalid Credentials")
+	ErrDuplicateEmail = errors.New("models: duplicate email")
+)
+
 
 type Snippet struct {
 	ID int

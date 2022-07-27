dd5d55d7410f48f1f7f43c1898433a7dc2d85e13
mahmud139 <mahmudulhasan139139@gmail.com>
2022-07-27 15:34:28 +0600

    update middleware handler

--- snippetbox/cmd/web/middleware.go
+++ snippetbox/cmd/web/middleware.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/justinas/nosurf"
 )
 
 func secureHeaders(next http.Handler) http.Handler {
@@ -53,4 +55,17 @@ func (app *application) requireAuthentication(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 
 	})
+}
+
+
+// Create a NoSurf middleware function which uses a customized CSRF cookie with 
+// the Secure, Path and HttpOnly flags set.
+func noSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path: "/",
+		Secure: true,
+	})
+	return csrfHandler
 }

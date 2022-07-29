70aa624634cb420571f2f1a7fd923f9faec9892c
mahmud139 <mahmudulhasan139139@gmail.com>
2022-07-29 22:28:58 +0600

    update TestHumandate function

--- snippetbox/cmd/web/templates_test.go
+++ snippetbox/cmd/web/templates_test.go
@@ -6,10 +6,52 @@ import (
 )
 
 func TestHumanDate(t *testing.T) {
+	/* Unit testing
 	tm := time.Date(2022, 07, 29, 07, 57, 0, 0, time.Local)
 	hd := humanDate(tm)
 
 	if hd != "29 Jul 2022 at 07:57" {
 		t.Errorf("want %q; got %q", "29 Jul 2022 at 07:57", hd)
+	} */
+
+	//Table-Driven testing
+	// Create a slice of anonymous structs containing the test case name, 
+	// input to our humanDate() function (the tm field), and expected output 
+	// (the want field).
+	tests := []struct {
+		name string
+		tm time.Time
+		want string
+	}{
+		{
+			name: "Local",
+			tm: time.Date(2022, 07, 29, 10, 15, 0, 0, time.Local),
+			want: "29 Jul 2022 at 10:15",
+		},
+		{
+			name: "Empty",
+			tm: time.Time{},
+			want: "",
+		}, 
+		{
+			name: "CET",
+			tm: time.Date(2022, 07, 29, 10, 15, 0, 0, time.FixedZone("CET", 4*60*60)),
+			want: "29 Jul 2022 at 06:15",
+		},
+	}
+
+	//loop over the test cases
+	for _, tt := range tests {
+		// Use the t.Run() function to run a sub-test for each test case. The 
+		// first parameter to this is the name of the test (which is used to 
+		// identify the sub-test in any log output) and the second parameter is 
+		// and anonymous function containing the actual test for each case.
+		t.Run(tt.name, func(t *testing.T) {
+			hd := humanDate(tt.tm)
+
+			if hd != tt.want {
+				t.Errorf("want %q; got %q", tt.want, hd)
+			}
+		})
 	}
 }

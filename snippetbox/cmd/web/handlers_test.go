db4751b1256461933359971c319e74657a5ae8e2
mahmud139 <mahmudulhasan139139@gmail.com>
2022-08-05 20:09:18 +0600

    using httptest.Server

--- snippetbox/cmd/web/handlers_test.go
+++ snippetbox/cmd/web/handlers_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -9,6 +10,7 @@ import (
 
 func TestPing(t *testing.T) {
 	// t.Parallel()
+	/* unit testing of handlers
 	//initialize a new httptest.NewRecorder
 	rr := httptest.NewRecorder()
 
@@ -42,6 +44,44 @@ func TestPing(t *testing.T) {
 
 	if string(body) != "OK" {
 		t.Errorf("want %q; got %q", "OK", string(body))
+	} */
+
+	// Create a new instance of our application struct. For now, this just 
+	// contains a couple of mock loggers (which discard anything written to them).
+	app := &application{
+		errorLog: log.New(ioutil.Discard, "", 0),
+		infoLog: log.New(ioutil.Discard, "", 0),
 	}
 
+	// We then use the httptest.NewTLSServer() function to create a new test 
+	// server, passing in the value returned by our app.routes() method as the 
+	// handler for the server. This starts up a HTTPS server which listens on a 
+	// randomly-chosen port of your local machine for the duration of the test. 
+	// Notice that we defer a call to ts.Close() to shutdown the server when 
+	// the test finishes.
+	ts := httptest.NewTLSServer(app.routes())
+	defer ts.Close()
+
+	// The network address that the test server is listening on is contained 
+	// in the ts.URL field. We can use this along with the ts.Client().Get() 
+	// method to make a GET /ping request against the test server. This 
+	// returns a http.Response struct containing the response.
+	rs, err := ts.Client().Get(ts.URL + "/ping")
+	if err != nil {
+		t.Fatal()
+	}
+
+	if rs.StatusCode != http.StatusOK {
+		t.Errorf("want %d; got %d", http.StatusOK, rs.StatusCode)
+	}
+
+	defer rs.Body.Close()
+	body, err := ioutil.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal()
+	}
+	
+	if string(body) != "OK" {
+		t.Errorf("want body to equal %q", "OK")
+	}
 }

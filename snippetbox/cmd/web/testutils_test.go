97fcd7a8898a2d621f9ab918c5f25204b81ea7ec
mahmud139 <mahmudulhasan139139@gmail.com>
2022-08-12 10:19:37 +0600

    testing post requests

--- snippetbox/cmd/web/testutils_test.go
+++ snippetbox/cmd/web/testutils_test.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httptest"
+	"net/url"
 	"regexp"
 	"testing"
 	"time"
@@ -105,3 +106,18 @@ func extractCSRFToken(t *testing.T, body []byte) string {
 
 	return html.UnescapeString(string(matches[1]))
 } 
+
+func (ts *testServer) postForm(t *testing.T, urlPath string, form url.Values) (int, http.Header, []byte) {
+	rs, err := ts.Client().PostForm(ts.URL+urlPath, form)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer rs.Body.Close()
+	body, err := ioutil.ReadAll(rs.Body)
+	if  err != nil {
+		t.Fatal(err)
+	}
+
+	return rs.StatusCode, rs.Header, body
+}

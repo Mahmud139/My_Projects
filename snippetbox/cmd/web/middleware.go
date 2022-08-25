d5a7a07dbd3b36873c2ec66b7eb85b9730bfd291
mahmud139 <mahmudulhasan139139@gmail.com>
2022-08-25 19:15:10 +0600

    redirect user appropriately after login

--- snippetbox/cmd/web/middleware.go
+++ snippetbox/cmd/web/middleware.go
@@ -46,6 +46,7 @@ func (app *application) requireAuthentication(next http.Handler) http.Handler {
 		// return from the middleware chain so that no subsequent handlers in
 		// the chain are executed.
 		if !app.isAuthenticated(r) {
+			app.session.Put(r, "redirectPathAfterLogin", r.URL.Path)
 			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 			return
 		}
